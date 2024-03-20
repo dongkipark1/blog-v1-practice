6dccfaf844ad9e5716723987aea8c0e73fdfb1a5
qkrehdrl4417 <qkrehdrl4417@naver.com>
2024-03-21 01:10:32 +0900

    게시글 수정

--- src/test/java/shop/mtcoding/blog/board/BoardNativeRepositoryTest.java
+++ src/test/java/shop/mtcoding/blog/board/BoardNativeRepositoryTest.java
@@ -16,6 +16,25 @@ public class BoardNativeRepositoryTest {
     private BoardNativeRepository boardNativeRepository;
 
     @Test
+    public void updateById_test() {
+
+        //given
+        int id = 1;
+        String title = "제목12";
+        String content = "내용12";
+        String username = "dad";
+        //when
+        boardNativeRepository.updateById(id, title, content, username);
+        //then
+        Board board = boardNativeRepository.findById(id);
+        System.out.println("updateById_test/board : " + board);
+        Assertions.assertThat(board.getTitle()).isEqualTo("제목12");
+        Assertions.assertThat(board.getContent()).isEqualTo("내용12");
+        Assertions.assertThat(board.getUsername()).isEqualTo("dad");
+    }
+
+
+    @Test
     public void deleteById_test() {
 
         //given
